63851e373108ec76e23620842006faeb0356b1e9
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2024-06-01 22:07:29 +0200

    FIX: bug bouton detail

--- immobilier.php
+++ immobilier.php
@@ -407,6 +407,10 @@ if ($result_favoris->num_rows > 0) {
         </div>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Popper.js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <!-- Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <script>
         $(document).ready(function() {
@@ -475,6 +479,12 @@ $(document).ready(function() {
         searchImmobiliers(inputVal);
     });
 
+    $('#immoModal').on('hidden.bs.modal', function () {
+        $(this).find('form').trigger('reset');
+        $(this).find('img').attr('src', '');
+        $(this).find('.modal-body p').text('');
+    });
+
     $(document).ready(function() {
 
     // Gestion des clics sur les boutons immo
@@ -537,5 +547,4 @@ function searchImmobiliers(inputVal) {
 <script src="js/bootstrap.min.js"></script>
 <script src="js/jquery.sticky.js"></script>
 <script src="js/main.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
